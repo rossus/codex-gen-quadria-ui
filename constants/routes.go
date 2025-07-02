006d6e4c2ca3f1b74076be022c95291db2c26ce2
Rostislav Pyshev <32388613+rossus@users.noreply.github.com>
2025-07-03 01:03:41 +0200

    Enable tile clicking for moves

--- constants/routes.go
+++ constants/routes.go
@@ -5,6 +5,7 @@ const (
 	RouteStart = "/start"
 	RouteGame  = "/game"
 	RouteDice  = "/dice/:value"
+	RouteMove  = "/move/:x/:y"
 
 	// CustomDiceDir is checked for user-provided dice images. Files should
 	// be named using the format "dice-<value>.svg".
